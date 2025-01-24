b0eca77b48ce84e86fa0a63679eb2374dd5b67ed
lucaperullo <lucaperullo@outlook.it>
2025-01-24 23:04:45 +0100

    chore: configure semantic-release

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import Checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({ parseNative: true })],
+  plugins: [
+    react(),
+    tsconfigPaths({ parseNative: true }),
+    Checker({ typescript: true }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
 });
